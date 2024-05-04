dc5b78a0399d254f205edf55223d12d6137e11fc
dIvYaJyOtIoP <139646426+dIvYaJyOtIoP@users.noreply.github.com>
2024-05-04 12:19:58 +0530

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -29,17 +29,17 @@ module.exports = {
 
   run: async (client, interaction) => {
     try {
-      const youtubeLink = 'https://discord.gg/xQF9f9yUEM';
-      const InstagramLink = 'https://www.instagram.com/rtxxgg/';
+      const youtubeLink = 'https://youtube.com/@djcodez';
+      const InstagramLink = 'https://www.instagram.com/ig_djcodes/';
       const { EmbedBuilder } = require('discord.js')
         const embed = new EmbedBuilder()
             .setColor('#da2a41')
             .setAuthor({
           name: 'Owner',
-          iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          iconURL: 'https://images-ext-1.discordapp.net/external/vVCH0wIz8urQKt6eHxsDWNJHIkhVIXy5M-liBxygVjc/https/cdn.discordapp.com/emojis/1236208075451666442.gif?width=144&height=144',
+          url: 'https://discord.gg/Tsuzz2zHXe'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [RTX GAMING](${youtubeLink})\n Instagram : 💙 [rtxxgg](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n\n Myself DivyaJyoti aka DJCodez. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [DJ CODEZ](${youtubeLink})\n Instagram : 💙 [DJ CODEZ](${InstagramLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
